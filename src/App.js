b00f0e91990fb6362704367d8bb3d60a70b19fab
Amen <krakenamen@gmail.com>
2025-11-23 22:00:49 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+"use client";
+import { useState, useRef } from "react";
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const inputRef = useRef(null);
+  const addTask = () => {
+    const text = inputRef.current.value;
+    const newTask = { completed: false, text };
+    setTasks([...tasks, newTask]);
+    inputRef.current.value = "";
+  }
+  const doneTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = !newTasks[index].completed;
+    setTasks(newTasks);
+  }
+  const deleteTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My To-Do List</h1>
+      <ul>
+        {
+          tasks.map((task, index) => (
+            <div className="f">
+              <li key={index} onClick={() => doneTask(index)}
+                className={task.completed ? "d" : "w"}>{task.text}
+              </li>
+              <span onClick={() => deleteTask(index)}>❌</span>
+            </div>
+          ))
+        }
+      </ul>
+      <input ref={inputRef} type="text" placeholder="Add a new task" />
+      <button onClick={addTask}>Add Task</button>
     </div>
   );
 }
